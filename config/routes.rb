015f4ffc095baa2ee80ddd41ea7180fcecdc6964
Leamsi Escribano <leamsi.esc@gmail.com>
2020-07-06 19:58:48 -0400

    Styling navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :reviews
   resources :users
 
-  
+  root "static#index"
+
 end
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
