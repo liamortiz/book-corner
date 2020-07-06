a43d542790f5d6c81a759e32d2d28db2894c362d
alexpac27 <alexpac27@gmail.com>
2020-07-06 17:23:00 -0400

    v1

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :books
   resources :reviews
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  
 end
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
