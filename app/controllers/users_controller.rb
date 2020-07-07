4775e72cf4938a18af89025a1199b1472ffe7875
Leamsi Escribano <leamsi.esc@gmail.com>
2020-07-07 13:32:10 -0400

    Working on sign in page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    byebug
+  end
+
+  def sign_in
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get "users/sign_in", to: "users#sign_in"
+
   resources :books
   resources :reviews
   resources :users
-  
+
 
   root "static#index"
 
