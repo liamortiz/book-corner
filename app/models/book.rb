5649748c93c9f1cd28452b288b44fe496fdedf3f
alexpac27 <alexpac27@gmail.com>
2020-07-07 01:16:39 -0400

    book show page and review form

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,22 @@
 class Book < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
+
+  def average_rating
+    if self.reviews.empty?
+      "N/A"
+    else
+      ratings = self.reviews.map{|review| review.rating}
+      total = ratings.sum.to_f / self.reviews.length.to_f
+      average = (total * 2).round / 2.0
+      answer = average.to_i == average ? average.to_i : average
+      "#{answer} out of 5"
+    end
+  end
+
+
+  
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :books
   resources :reviews
   resources :users
+  
 
   root "static#index"
 
