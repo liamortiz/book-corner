9b808cae39c1a5774c0deffa99ce8fa7e923f307
Leamsi Escribano <leamsi.esc@gmail.com>
2020-07-08 12:36:46 -0400

    Fixing browse page bugs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,33 @@
 class BooksController < ApplicationController
 
     def index
-      @books = GoogleBooks.search(self.query, {count: 40})
+      # Filter the search results from the API via page index and query
+      @books = GoogleBooks.search(self.query, {count: 40,
+        page: params[:page] ? params[:page].to_i : 1})
+      # Remove any that doesn't have ISBNS
+      @books = @books.select{|book| book.isbn}
     end
 
     def show
-       @book = Book.find_by(isbn: params[:isbn])
-
+      # If the book is in the databse do nothing
+      # Else add the book to the databse
+      @book = Book.find_by(isbn: params[:isbn])
       unless @book
         @book = GoogleBooks.search(isbn: params[:isbn]).first
         @book = Book.create(title: @book.title, description: @book.description, authors: @book.authors,
           published_date: @book.published_date, isbn: @book.isbn, categories: @book.categories,
-          average_rating: @book.average_rating, ratings_count: @book.ratings_count, image_link:@book.image_link)
+          average_rating: @book.average_rating, ratings_count: @book.ratings_count, image_link: @book.image_link)
 
         @book.create_reviews
       end
     end
 
     def search
-      redirect_to "/books/?q=#{self.query}"
+      redirect_to "/books/?q=#{self.query}&page=1"
     end
 
     def query
+      # Gets the query from the url
       if params[:q]
         return params[:q]
       else
