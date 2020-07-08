8fef3b46e824411b19605f8222b906c936f0e573
alexpac27 <alexpac27@gmail.com>
2020-07-08 11:33:07 -0400

    create logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :user_shelves
   has_many :books, through: :user_shelves
   has_many :reviewed_books, through: :reviews, source: :book
+
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
 end
