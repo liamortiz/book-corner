bf572f9d8174cb48110213c6b5f5b279c94588df
Leamsi Escribano <leamsi.esc@gmail.com>
2020-07-08 21:11:29 -0400

    working on user show page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
 
 
-  resources :reviews, only: [:create]
+  resources :reviews, only: [:create, :destroy]
   resources :users, only: [:create, :show]
   resources :user_shelves, only: [:create, :destroy]
 
